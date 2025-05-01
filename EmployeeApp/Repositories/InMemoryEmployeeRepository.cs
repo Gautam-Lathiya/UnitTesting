df9c59373e68fa5608c823caf457d0e9276c7035
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-05-01 18:55:50 +0530

    Add project files.

---
+++ EmployeeApp/Repositories/InMemoryEmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeApp.Models;
+
+namespace EmployeeApp.Repositories
+{
+    public class InMemoryEmployeeRepository : IEmployeeRepository
+    {
+        private readonly List<Employee> _employees = new();
+
+        public void Add(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employees;
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employees.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
